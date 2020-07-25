bbf88aa27ad001f1294cc5af62f692136960781a
nrmarsh36 <nrmarsh36@gmail.com>
2020-07-25 13:49:03 -0500

    artist seach functional

--- server.js
+++ server.js
@@ -51,8 +51,20 @@ function initialPrompts() {
 }
 
 function artistSearch() {
-    console.log('Searching artist...')
-    initialPrompts()
+    inquirer.prompt([{
+        message: 'Which artist are you looking for?',
+        name: 'artist',
+    }]).then(answer => {
+        connection.query(
+            'SELECT position, artist, song, year FROM top5000 WHERE ?',
+            {artist: answer.artist}, 
+            (err, results) => {
+                if (err) throw err
+                console.table(results)
+                initialPrompts()
+            }
+        )
+    })
 };
 
 //searches artists showing up multiple times
