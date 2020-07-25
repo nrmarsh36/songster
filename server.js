982e23da4a16b8883325d7a37e3938f6dda38180
nrmarsh36 <nrmarsh36@gmail.com>
2020-07-25 13:31:43 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const inquirer = require('inquirer')
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'top_songsDB',
+});
+
+connection.connect(err => {
+    if (err) throw err
+    console.log(`Connected to mysql as id ${connection.threadId}`)
+    initialPrompts()
+});
+
+function initialPrompts() {
+    inquirer.prompt([
+        {
+            name: 'action',
+            message: 'What do you want to do?',
+            type: 'list',
+            choices: [
+                'ARTIST SEARCH', 
+                'MULTI SEARCH', 
+                'RANGE SEARCH', 
+                'SONG SEARCH', 
+                'EXIT'
+            ]
+        }
+    ]).then(answer => {
+        switch(answer.action) {
+            case 'ARTIST SEARCH':
+                artistSearch()
+                break
+            case 'MULTI SEARCH':
+                multiSearch()
+                break   
+            case 'RANGE SEARCH':
+                rangeSearch()
+                break   
+            case 'SONG SEARCH':
+                songSearch()
+                break      
+            default:
+                connection.end()
+                process.exit()
+        }
+    })
+}
+
+function artistSearch() {
+    console.log('Searching artist...')
+    initialPrompts()
+};
+
+//searches artists showing up multiple times
+function multiSearch() {
+    console.log('Multi search...')
+    initialPrompts()
+};
+
+//display data within a certain range
+function rangeSearch() {
+    console.log('Range search...')
+    initialPrompts()
+};
+
+function songSearch() {
+    console.log('Searching songs...')
+    initialPrompts()
+};
+
+
